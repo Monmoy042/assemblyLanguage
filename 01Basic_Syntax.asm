467ad73035b5e59187f24f2bc650538dfd60c4b8
Monmoy042 <monmoykms.16@gmail.com>
2019-08-07 12:23:23 +0600

    Basic Syntax of assembly language coding has added

---
+++ 01Basic_Syntax.asm
@@ -0,0 +1,17 @@
+.MODEL SMALL ;Declare Model
+.STACK 100H  
+
+.DATA
+    ;Declare varible under the section of data 
+    
+.CODE
+    ;Write the codes here
+
+MAIN PROC ;Start the main procedure
+    ;Codes statements
+    ;Codes statements
+    
+    MOV AH,4CH
+    INT 21H
+    MAIN ENDP 
+END MIAN
